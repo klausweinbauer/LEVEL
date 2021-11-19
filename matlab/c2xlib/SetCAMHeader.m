73d79b9a789feaabbbaa442d334dc8e2f1e95025
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-11-19 09:26:47 +0100

    Finish Get & Set CAM header

--- matlab/c2xlib/SetCAMHeader.m
+++ matlab/c2xlib/SetCAMHeader.m
@@ -17,7 +17,7 @@ classdef SetCAMHeader < matlab.System & coder.ExternalDependency
         function [] = stepImpl(obj, StationID, ProtocolVersion, MessageID) 
             if coder.target('Rtw') || coder.target('Sfun') 
                 err = int32(0);
-                err = coder.ceval('setCAMHeader', StationID, ProtocolVersion, MessageID);
+                err = coder.ceval('setCAMHeader', StationID, int32(ProtocolVersion), int32(MessageID));
                 obj.printErrorCode(err);
             end            
         end
@@ -26,34 +26,34 @@ classdef SetCAMHeader < matlab.System & coder.ExternalDependency
         end
 
         function printErrorCode(~, err)
-   if (err == -22)
-       error('ERR_CAM_ALREADY_EXISTS')
-   elseif (err == -21)
-       error('ERR_LOW_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
-   elseif (err == -20)
-       error('ERR_HIGH_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
-   elseif (err == -40)
-       error('ERR_DENM_ALREADY_EXISTS')
-   elseif (err == -1)
-       error('ERR_MSG_NOT_FOUND')
-   elseif (err == -9)
-       error('ERR_ARG_NULL')
-   elseif (err == -8)
-       error('ERR_TRANSMITTER_START')
-   elseif (err == -7)
-       error('ERR_RECEIVER_START')
-   elseif (err == -6)
-       error('ERR_DECODE')
-   elseif (err == -5)
-       error('ERR_ENCODE')
-   elseif (err == -4)
-       error('ERR_BUFFER_OVERFLOW')
-   elseif (err == -3)
-       error('ERR_NULL')
-   elseif (err == -2)
-       error('ERR_ALLOC_FAILED')
-   end
-end
+            if (err == -22)
+                error('ERR_CAM_ALREADY_EXISTS')
+            elseif (err == -21)
+                error('ERR_LOW_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
+            elseif (err == -20)
+                error('ERR_HIGH_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
+            elseif (err == -40)
+                error('ERR_DENM_ALREADY_EXISTS')
+            elseif (err == -1)
+                error('ERR_MSG_NOT_FOUND')
+            elseif (err == -9)
+                error('ERR_ARG_NULL')
+            elseif (err == -8)
+                error('ERR_TRANSMITTER_START')
+            elseif (err == -7)
+                error('ERR_RECEIVER_START')
+            elseif (err == -6)
+                error('ERR_DECODE')
+            elseif (err == -5)
+                error('ERR_ENCODE')
+            elseif (err == -4)
+                error('ERR_BUFFER_OVERFLOW')
+            elseif (err == -3)
+                error('ERR_NULL')
+            elseif (err == -2)
+                error('ERR_ALLOC_FAILED')
+            end
+            end
 
     end
     methods (Static)
@@ -76,7 +76,7 @@ end
             % Parametrize library extension
             libName =  strcat('c2xcam', linkLibExt);
             % Other linking parameters
-            libPath = './';
+            libPath = 'C:\Program Files\Polyspace\R2021a\extern\lib\win64\c2x';
             libPriority = '';
             libPreCompiled = true;
             libLinkOnly = true;
