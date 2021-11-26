679b484554a71555a2be6dff5db05f3df5c4dfd0
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-11-26 09:00:11 +0100

    Add stationary vehicle container getter matlab classes

---
+++ matlab/c2xlib/GetDENMStationaryVehicleContainerCarryingDangerousGoodsCompanyName.m
@@ -0,0 +1,111 @@
+classdef GetDENMStationaryVehicleContainerCarryingDangerousGoodsCompanyName < matlab.System & coder.ExternalDependency
+    
+    properties (Nontunable)
+        CompanyNameSize = int32(16);
+    end
+    
+    properties (Hidden)
+    end
+    
+    properties (Access = protected)
+    end
+
+    methods (Access = protected)
+        function setupImpl(~)
+        end
+        
+        function [CompanyName] = stepImpl(obj, StationID, SequenceNumber) 
+            if coder.target('Rtw') || coder.target('Sfun') 
+                err = int32(0);
+                CompanyName = uint8(zeros(obj.CompanyNameSize, 1));
+                coder.cinclude('c2xdenm.h');
+                err = coder.ceval('getDENMStationaryVehicleContainerCarryingDangerousGoodsCompanyName', ...
+                    StationID, SequenceNumber, coder.ref(CompanyName), length(CompanyName));
+                obj.printErrorCode(err);
+            end            
+        end
+        
+        function releaseImpl(~)            
+        end
+
+        function printErrorCode(~, err)
+            if (err == -22)
+                error('ERR_CAM_ALREADY_EXISTS')
+            elseif (err == -21)
+                error('ERR_LOW_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
+            elseif (err == -20)
+                error('ERR_HIGH_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
+            elseif (err == -40)
+                error('ERR_DENM_ALREADY_EXISTS')
+            elseif (err == -1)
+                error('ERR_MSG_NOT_FOUND')
+            elseif (err == -9)
+                error('ERR_ARG_NULL')
+            elseif (err == -8)
+                error('ERR_TRANSMITTER_START')
+            elseif (err == -7)
+                error('ERR_RECEIVER_START')
+            elseif (err == -6)
+                error('ERR_DECODE')
+            elseif (err == -5)
+                error('ERR_ENCODE')
+            elseif (err == -4)
+                error('ERR_BUFFER_OVERFLOW')
+            elseif (err == -3)
+                error('ERR_NULL')
+            elseif (err == -2)
+                error('ERR_ALLOC_FAILED')
+            end
+        end
+
+    end
+    methods (Static)
+        function bName = getDescriptiveName(~)
+            bName = 'GetDENMStationaryVehicleContainerCarryingDangerousGoodsCompanyName';
+        end
+        
+        function supported = isSupportedContext(buildContext)
+            myTarget = {'rtw'};
+            if  buildContext.isCodeGenTarget(myTarget)
+                supported = true;
+            else
+                error('API only supported for mex, lib, exe, dll');
+            end
+        end
+        
+        function updateBuildInfo(buildInfo, buildContext)
+            [~, linkLibExt, execLibExt, ~] = buildContext.getStdLibInfo();
+
+            % Parametrize library extension
+            libName =  strcat('c2xdenm', linkLibExt);
+            % Other linking parameters
+            libPath = 'C:\Program Files\Polyspace\R2021a\extern\lib\win64\c2x';
+            libPriority = '';
+            libPreCompiled = true;
+            libLinkOnly = true;
+
+            % Linking command
+            buildInfo.addLinkObjects(libName,libPath,libPriority,libPreCompiled,libLinkOnly);
+            buildInfo.addIncludePaths(libPath);
+            buildInfo.addIncludeFiles('c2xcommon.h');
+        end
+    end
+
+    methods (Access = protected)
+        function [CompanyName] = getOutputSizeImpl(obj)
+            CompanyName = [obj.CompanyNameSize 1];
+        end 
+        
+        function [CompanyName] = isOutputFixedSizeImpl(obj)
+            CompanyName = true;
+        end
+        
+        function [CompanyName] = getOutputDataTypeImpl(obj)
+            CompanyName = 'uint8';
+        end
+        
+        function [CompanyName] = isOutputComplexImpl(obj)
+            CompanyName = false;
+        end
+    end
+end
---
+++ matlab/c2xlib/GetDENMStationaryVehicleContainerVehicleIdentification.m
@@ -0,0 +1,117 @@
+classdef GetDENMStationaryVehicleContainerVehicleIdentification < matlab.System & coder.ExternalDependency
+    
+    properties (Nontunable)
+        WMInumberSize = int32(16);
+        VDSSize = int32(16);
+    end
+    
+    properties (Hidden)
+    end
+    
+    properties (Access = protected)
+    end
+
+    methods (Access = protected)
+        function setupImpl(~)
+        end
+        
+        function [WMInumber, VDS] = stepImpl(obj, StationID, SequenceNumber) 
+            if coder.target('Rtw') || coder.target('Sfun') 
+                err = int32(0);
+                WMInumber = uint8(zeros(obj.WMInumberSize, 1));
+                VDS = uint8(zeros(obj.VDSSize, 1));
+                coder.cinclude('c2xdenm.h');
+                err = coder.ceval('getDENMStationaryVehicleContainerVehicleIdentification', StationID, SequenceNumber, ...
+                    coder.ref(WMInumber), coder.ref(VDS));
+                obj.printErrorCode(err);
+            end            
+        end
+        
+        function releaseImpl(~)            
+        end
+
+        function printErrorCode(~, err)
+            if (err == -22)
+                error('ERR_CAM_ALREADY_EXISTS')
+            elseif (err == -21)
+                error('ERR_LOW_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
+            elseif (err == -20)
+                error('ERR_HIGH_FREQ_CONTAINER_TYPE_BASIC_VEHICLE')
+            elseif (err == -40)
+                error('ERR_DENM_ALREADY_EXISTS')
+            elseif (err == -1)
+                error('ERR_MSG_NOT_FOUND')
+            elseif (err == -9)
+                error('ERR_ARG_NULL')
+            elseif (err == -8)
+                error('ERR_TRANSMITTER_START')
+            elseif (err == -7)
+                error('ERR_RECEIVER_START')
+            elseif (err == -6)
+                error('ERR_DECODE')
+            elseif (err == -5)
+                error('ERR_ENCODE')
+            elseif (err == -4)
+                error('ERR_BUFFER_OVERFLOW')
+            elseif (err == -3)
+                error('ERR_NULL')
+            elseif (err == -2)
+                error('ERR_ALLOC_FAILED')
+            end
+        end
+
+    end
+    methods (Static)
+        function bName = getDescriptiveName(~)
+            bName = 'GetDENMStationaryVehicleContainerVehicleIdentification';
+        end
+        
+        function supported = isSupportedContext(buildContext)
+            myTarget = {'rtw'};
+            if  buildContext.isCodeGenTarget(myTarget)
+                supported = true;
+            else
+                error('API only supported for mex, lib, exe, dll');
+            end
+        end
+        
+        function updateBuildInfo(buildInfo, buildContext)
+            [~, linkLibExt, execLibExt, ~] = buildContext.getStdLibInfo();
+
+            % Parametrize library extension
+            libName =  strcat('c2xdenm', linkLibExt);
+            % Other linking parameters
+            libPath = 'C:\Program Files\Polyspace\R2021a\extern\lib\win64\c2x';
+            libPriority = '';
+            libPreCompiled = true;
+            libLinkOnly = true;
+
+            % Linking command
+            buildInfo.addLinkObjects(libName,libPath,libPriority,libPreCompiled,libLinkOnly);
+            buildInfo.addIncludePaths(libPath);
+            buildInfo.addIncludeFiles('c2xcommon.h');
+        end
+    end
+
+    methods (Access = protected)
+        function [WMInumber, VDS] = getOutputSizeImpl(obj)
+            WMInumber = [obj.WMInumberSize 1];
+            VDS = [obj.VDSSize 1];
+        end 
+        
+        function [WMInumber, VDS] = isOutputFixedSizeImpl(obj)
+            WMInumber = true;
+            VDS = true;
+        end
+        
+        function [WMInumber, VDS] = getOutputDataTypeImpl(obj)
+            WMInumber = 'uint8';
+            VDS = 'uint8';
+        end
+        
+        function [WMInumber, VDS] = isOutputComplexImpl(obj)
+            WMInumber = false;
+            VDS = false;
+        end
+    end
+end
