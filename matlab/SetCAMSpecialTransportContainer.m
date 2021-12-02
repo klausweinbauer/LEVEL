68efc9e204230cd2735c152e4d98301cdc4f5b31
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-02 13:28:42 +0100

    Add matlab classes

---
+++ matlab/SetCAMSpecialTransportContainer.m
@@ -0,0 +1,74 @@
+classdef SetCAMSpecialTransportContainer < matlab.System & coder.ExternalDependency
+    
+    properties (Nontunable)
+          
+    end
+    
+    properties (Hidden)
+    end
+    
+    properties (Access = protected)
+    end
+
+    methods (Access = protected)
+        function setupImpl(~)
+        end
+        
+        function [] = stepImpl(obj, StationID, SpecialTransportType, LightBarSirenInUse) 
+            if coder.target('Rtw') || coder.target('Sfun') 
+                err = int32(0);
+                TmpSpecialTransportType = uint8(SpecialTransportType);
+                TmpLightBarSirenInUse = uint8(LightBarSirenInUse);
+                coder.cinclude('c2xcam.h');
+                err = coder.ceval('setCAMSpecialTransportContainer', StationID, coder.ref(TmpSpecialTransportType), ...
+                    length(TmpSpecialTransportType), coder.ref(TmpLightBarSirenInUse), length(TmpLightBarSirenInUse));
+                obj.printErrorCode(err);
+            end            
+        end
+        
+        function releaseImpl(~)            
+        end
+
+        function printErrorCode(~, err)
+            if (err ~= 0)
+                MsgBytes = uint8(zeros(255, 1));
+                MsgLength = int32(0);
+                coder.ceval('getLastErrMsg', coder.ref(MsgBytes), length(MsgBytes), coder.ref(MsgLength));
+                Msg = ascii2str(MsgBytes);
+                error(Msg);
+            end
+        end
+
+    end
+    methods (Static)
+        function bName = getDescriptiveName(~)
+            bName = 'SetCAMSpecialTransportContainer';
+        end
+        
+        function supported = isSupportedContext(buildContext)
+            myTarget = {'rtw'};
+            if  buildContext.isCodeGenTarget(myTarget)
+                supported = true;
+            else
+                error('API only supported for mex, lib, exe, dll');
+            end
+        end
+        
+        function updateBuildInfo(buildInfo, buildContext)
+            [~, linkLibExt, execLibExt, ~] = buildContext.getStdLibInfo();
+
+            % Parametrize library extension
+            libName =  strcat('c2xcam', linkLibExt);
+            % Other linking parameters
+            libPath = 'C:\Program Files\Polyspace\R2021a\extern\lib\win64\c2x';
+            libPriority = '';
+            libPreCompiled = true;
+            libLinkOnly = true;
+
+            % Linking command
+            buildInfo.addLinkObjects(libName,libPath,libPriority,libPreCompiled,libLinkOnly);
+            buildInfo.addIncludePaths(libPath);
+            buildInfo.addIncludeFiles('c2xcommon.h');
+        end
+    end
+end
