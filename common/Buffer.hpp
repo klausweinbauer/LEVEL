d2b153f470561a0a5fed1e533472f6d9ed4e961b
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2022-06-30 10:18:33 +0200

    Make buffer destructor virtual

--- common/Buffer.hpp
+++ common/Buffer.hpp
@@ -5,13 +5,14 @@
 #include <stdlib.h>
 
 namespace c2x {
-    class Buffer
-    {
-    public:
-        virtual size_t size() = 0;
-        virtual size_t write(const void* src, size_t size,
-            void* application_specific_key) = 0;
-        virtual size_t copy(void* buffer, size_t size) = 0;
-        virtual void reset() = 0;
-    };
-}
+class Buffer {
+public:
+  virtual ~Buffer();
+
+  virtual size_t size() = 0;
+  virtual size_t write(const void *src, size_t size,
+                       void *application_specific_key) = 0;
+  virtual size_t copy(void *buffer, size_t size) = 0;
+  virtual void reset() = 0;
+};
+} // namespace c2x
