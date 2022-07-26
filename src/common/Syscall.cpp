23052c1a506815143b428be59d133f19a36090f4
Klaus Weinbauer <weinbauer.klaus@gmail.com>
2022-07-26 20:43:39 +0200

    Add windows syscalls

--- src/common/Syscall.cpp
+++ src/common/Syscall.cpp
@@ -1,33 +1,62 @@
 #include <Syscall.hpp>
 
 #ifdef _WIN32
+#include <WinSock2.h>
 #elif __linux__
 #include <sys/socket.h>
 #endif
 
 namespace level {
 
-int Syscall::posixPoll(struct pollfd *fds, nfds_t nfds, int timeout) {
+int Syscall::posixPoll(pollfd_l *fds, nfds_l nfds, int timeout) {
 #ifdef _WIN32
-
+  return WSAPoll((pollfd*)fds, nfds, timeout);
 #elif __linux__
   return poll(fds, nfds, timeout);
 #endif
 }
 
 int Syscall::posixSocket(int domain, int type, int protocol) {
-#ifdef _WIN32
-#elif __linux__
+#if defined(_WIN32) || defined(__linux__)
   return socket(domain, type, protocol);
 #endif
 }
 
-ssize_t Syscall::posixSendTo(int sockfd, const void *buf, size_t len, int flags,
-                             const struct sockaddr *dest_addr,
-                             socklen_t addrlen) {
+ssize_t Syscall::posixSendTo(int sockfd, const void *buf, size_t len, int flags, const sockaddr_l *dest_addr, socklen_l addrlen) {
 #ifdef _WIN32
+  return sendto(sockfd, (char*)buf, len, flags, (sockaddr*)dest_addr, addrlen);
 #elif __linux__
   return sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+#endif
+}
+
+int Syscall::posixSetSockOpt(int socket, int level, int option_name,
+                 const void *option_value, socklen_l option_len)
+{
+#ifdef _WIN32
+  return setsockopt(socket, level, option_name, (char*)option_value, option_len);
+#elif __linux__
+#endif
+}
+
+int Syscall::posixClose(int fd) {
+#ifdef _WIN32
+  return closesocket(fd);
+#elif __linux__
+#endif
+}
+
+int Syscall::posixBind(int sockfd, const sockaddr_l *addr, socklen_l addrlen) {
+#ifdef _WIN32
+  return bind(sockfd, (sockaddr*)addr, addrlen);
+#elif __linux__
+#endif
+}
+
+ssize_t Syscall::posixRecvFrom(int sockfd, void *buf, size_t len, int flags, sockaddr_l *src_addr, socklen_l addrlen) {
+#ifdef _WIN32
+  return recvfrom(sockfd, (char*)buf, len, flags, (sockaddr*)src_addr, (int*)addrlen);
+#elif __linux__
 #endif
 }
 
--- src/common/Syscall.hpp
+++ src/common/Syscall.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #ifdef _WIN32
-
 #elif __linux__
 #include <poll.h>
 #include <sys/socket.h>
@@ -12,13 +11,20 @@
 namespace level {
 
 #ifdef _WIN32
-typedef unsigned long int nfds_t;
-typedef unsigned int socklen_t;
+typedef unsigned long nfds_l;
+typedef unsigned int socklen_l;
+typedef long int ssize_t;
 
-struct pollfd {
+
+struct pollfd_l {
   int fd;
   short events;
-  short revents;
+  short revents; 
+};
+
+struct sockaddr_l {
+  unsigned short int sa_family;
+  char sa_data[14];
 };
 #elif __linux__
 #endif
@@ -36,7 +42,7 @@ class Syscall {
    * revents fields. Zero indicates the system call timed out, and -1 is
    * returned on error.
    */
-  int posixPoll(struct pollfd *fds, nfds_t nfds, int timeout);
+  int posixPoll(pollfd_l *fds, nfds_l nfds, int timeout);
 
   /**
    * @brief Creates an endpoint for communication and returns a file descriptor
@@ -65,7 +71,51 @@ class Syscall {
    * error, -1 is returned.
    */
   ssize_t posixSendTo(int sockfd, const void *buf, size_t len, int flags,
-                      const struct sockaddr *dest_addr, socklen_t addrlen);
+                      const sockaddr_l *dest_addr, socklen_l addrlen);
+
+  /**
+  * @brief Sets socket options.
+  *
+  * @param socket File descriptor to set the options.
+  * @param level Specifies the protocol level at which the option resides.
+  * @param option_name Specifies a single option to set.
+  * @param option_value 
+  * @param option_len
+  * @return int 0 is returned on successful completion and -1 otherwise.
+  */
+  int posixSetSockOpt(int socket, int level, int option_name,
+                 const void *option_value, socklen_l option_len);
+
+  /**
+   * @brief Closes a file descriptor.
+   * 
+   * @param fd File descriptor to close.
+   * @return int Returns zero on success and -1 on error.
+   */
+  int posixClose(int fd);
+
+  /**
+   * @brief Assigns the address specified by addr to the socket referred to by the file descriptor.
+   * 
+   * @param sockfd Socket file descriptor.
+   * @param addr Address to assign.
+   * @param addrlen Size of the address.
+   * @return int Returns zero on success and -1 on error.
+   */
+  int posixBind(int sockfd, const sockaddr_l *addr, socklen_l addrlen);
+
+  /**
+   * @brief Used to receive messages from a socket.
+   * 
+   * @param sockfd Socket file descriptor.
+   * @param buf Buffer for the received message.
+   * @param len Length of the buffer.
+   * @param flags Special flags.
+   * @param src_addr Source address from where the message was received.
+   * @param addrlen This is a value-result argument. Before the call, it should be initialized to the size of the buffer associated with src_addr. Upon return, addrlen is updated to contain the actual size of the source address.
+   * @return ssize_t Returns the number of bytes received, or -1 if an error occurred.
+   */
+  ssize_t posixRecvFrom(int sockfd, void *buf, size_t len, int flags, sockaddr_l *src_addr, socklen_l addrlen);
 };
 
 } // namespace level
